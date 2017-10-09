4f568020c524f30d2a510e4276eabbd724c2dc18
pjumrustanasan <pj4@williams.edu>
2017-10-09 12:30:37 -0400

    combined bucketize and bucketize_2 into one test file

--- tests/testthat/test_bucketize_D.R
+++ tests/testthat/test_bucketize_D.R
@@ -5,6 +5,7 @@ data_vector <- c(runif(10, min = -10, max = 10))
 row_factor <- as.factor(base::sample(x = c("row1", "row2", "row3"), size = 10, replace = TRUE))
 col_factor <- as.factor(base::sample(x = c("col1", "col2", "col3"), size = 10, replace = TRUE))
 
+
 test_that("bucketize performs what is supposed to", {
   
   # expected result
@@ -15,6 +16,33 @@ test_that("bucketize performs what is supposed to", {
   
   # expect both results to be identical
   expect_identical(expect, result)
+})
+
+
+test_that("bucketize works on matix input", {
+  
+  ## create a matrx to be tested
+  data_matrix <- runif(12, min = -10, max = 10)
+  data_matrix[sample(c(1:12), size = 3)] = NA
+  dim(data_matrix) <- c(3,4)
+  
+  row_factor <- base::sample(x = c("row1", "row2", "row3"), size = 12, replace = TRUE)
+  row_factor[sample(c(1:12), size = 2)] = NA
+  row_factor <- as.factor(row_factor)
+  
+  col_factor <- base::sample(x = c("col1", "col2", "col3"), size = 12, replace = TRUE)
+  col_factor[sample(c(1:12), size = 2)] = NA
+  col_factor <- as.factor(col_factor)
+  
+  # what is expected
+  expect <- tapply(data_matrix, list(col_factor, row_factor), FUN = mean)
+  
+  # result from the function
+  result <- backtest:::bucketize(data_matrix, row_factor, col_factor, compute = mean)
+  
+  # expect both results to be identical
+  expect_identical(expect, result)
+  
 })
 
 
